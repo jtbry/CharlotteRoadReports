514110e4557200e2c6ed1ffc344a2017b908e336
jtbry <justintbry@gmail.com>
2021-04-29 02:05:52 -0400

    fix(db): change DSN style

--- models/database.go
+++ models/database.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"fmt"
 	"os"
 
 	"gorm.io/driver/postgres"
@@ -12,7 +11,7 @@ var DB *gorm.DB
 
 // Initialize database connection and migrations
 func DatabaseInit() {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	dsn := os.Getenv("DATABASE_URL")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
