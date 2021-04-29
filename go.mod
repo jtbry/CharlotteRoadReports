dc0630aa71a64eb58e13d530344c886a33755f69
jtbry <justintbry@gmail.com>
2021-04-29 03:05:01 -0400

    feat: use New Relic in production

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/newrelic/go-agent v3.11.0+incompatible
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	gorm.io/driver/postgres v1.0.8
 	gorm.io/gorm v1.21.9
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/jtbry/CharlotteRoadReports/models"
 	"github.com/jtbry/CharlotteRoadReports/routes"
+	newrelic "github.com/newrelic/go-agent"
+	"github.com/newrelic/go-agent/_integrations/nrgin/v1"
 )
 
 func main() {
@@ -15,6 +17,10 @@ func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("Error loading .env file")
+	}
+	env := os.Getenv("ENV")
+	if env == "" {
+		env = "development"
 	}
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -25,7 +31,23 @@ func main() {
 	go BeginPolling()
 
 	// Create web app.
-	web := gin.Default()
+	web := gin.New()
+	web.Use(gin.Recovery())
+	if env == "development" {
+		web.Use(gin.Logger())
+	} else {
+		nrlicense := os.Getenv("NEW_RELIC_LICENSE_KEY")
+		if nrlicense != "" {
+			nrcfg := newrelic.NewConfig("CharlotteRoadReports", nrlicense)
+			app, err := newrelic.NewApplication(nrcfg)
+			if err != nil {
+				log.Printf("%s\nUnable to create New Relic app\n", err)
+			} else {
+				web.Use(nrgin.Middleware(app))
+			}
+		}
+	}
+
 	api := web.Group("/api")
 	routes.RegisterIncidentsApi(api)
 	web.Run(":" + port)
