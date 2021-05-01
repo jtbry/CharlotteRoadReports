ab1c20756c622ace33758893a9bf58b1a0505f14
jtbry <justintbry@gmail.com>
2021-05-01 18:02:22 -0400

    feat(web): add a 404 page

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -6,6 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func (s *Server) handleNoRoute() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.HTML(http.StatusNotFound, "no_route.tmpl.html", nil)
+	}
+}
+
 func (s *Server) handleIndex() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "index.tmpl.html", gin.H{
--- pkg/app/routes.go
+++ pkg/app/routes.go
@@ -2,4 +2,5 @@ package app
 
 func (s *Server) setRoutes() {
 	s.router.GET("/", s.handleIndex())
+	s.router.NoRoute(s.handleNoRoute())
 }
